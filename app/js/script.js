690b83076a33e0afd7c191c347ddec4d14e52639
Dev <dvptl68@gmail.com>
2020-05-06 16:20:22 -0400

    Create log in/sign up pages

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,39 @@
 let buttons = document.getElementsByClassName('intro');
+
 for (let i = 0; i < buttons.length; i++){
-    buttons.item(i).style.transition = 'color 0.4s, background-color 0.4s';
+  buttons.item(i).style.transitionDuration = '0.4s';
 }
+
+let welcome = document.getElementById('welcome-div');
+let info = document.getElementById('info-div');
+let login = buttons.item(0);
+let signup = buttons.item(1);
+let infoBox = document.getElementsByClassName('info');
+let passConf = document.getElementById('passConf');
+let back = document.getElementById('back');
+let submit = document.getElementById('submit');
+function startProgram(event){
+  login.style.cursor = 'default';
+  signup.style.cursor = 'default';
+  welcome.style.transitionDuration = '1s';
+  welcome.style.opacity = '0';
+  info.style.transitionDuration = '1s';
+  info.style.display = 'inline-block';
+  for (let i = 0; i < infoBox.length; i++){
+    infoBox.item(i).style.display = 'block';
+  }
+  if (event.target.isSameNode(login)){
+    infoBox.item(0).style.display = 'none';
+    infoBox.item(1).style.display = 'none';
+    infoBox.item(4).style.display = 'none';
+  }
+  back.style.display = 'inline-block';
+  submit.style.display = 'inline-block';
+  setTimeout(function(){
+    welcome.style.display = 'none';
+    info.style.opacity = '1';
+  }, 1000);
+}
+
+login.addEventListener('click', startProgram);
+signup.addEventListener('click', startProgram);
--- main.js
+++ main.js
@@ -5,34 +5,34 @@ let window;
 
 function createWindow(){
 
-    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-    window = new BrowserWindow({
-        width: Math.floor(width * 0.8),
-        height: Math.floor(height * 0.9),
-        icon: 'app/images/icon.png',
-        show: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  window = new BrowserWindow({
+    width: Math.floor(width * 0.8),
+    height: Math.floor(height * 0.9),
+    icon: 'app/images/icon.png',
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-    window.loadFile('app/html/index.html');
-    // window.removeMenu();
+  window.loadFile('app/html/index.html');
+  // window.removeMenu();
 
-    window.on('ready-to-show', function(){
-        window.show();
-        window.focus();
-    });
+  window.on('ready-to-show', function(){
+    window.show();
+    window.focus();
+  });
 }
 
 app.on('windows-all-closed', function(){
-    if (process.platform !== 'darwin'){
-        app.quit();
-    }
+  if (process.platform !== 'darwin'){
+    app.quit();
+  }
 });
 
 app.on('ready', function(){
-    if (BrowserWindow.getAllWindows().length === 0){
-        createWindow();
-    }
+  if (BrowserWindow.getAllWindows().length === 0){
+    createWindow();
+  }
 });
